be25c0e522bd95861198ce8fe72d45c516521cf6
vaarg <104344158+vaarg@users.noreply.github.com>
2023-12-02 14:36:07 +1100

    Update day_1B.py

--- day_1B.py
+++ day_1B.py
@@ -1,6 +1,6 @@
 import re
 
-def replaceStringsWithNumbers(line):
+def convert_to_numeric(line):
     replacements = {
         "one": "o1e", "two": "t2o", "three": "t3e", "four": "f4r", 
         "five": "f5e", "six": "s6x", "seven": "s7n", "eight": "e8t", "nine": "n9e"
@@ -12,7 +12,7 @@ def replaceStringsWithNumbers(line):
 total = 0
 with open("day_1.txt", "r") as file:
     for line in file:
-        line = replaceStringsWithNumbers(line)
+        line = convert_to_numeric(line)
         line = re.sub("[a-z]", "", line.strip())
         merged = int(line[0] + line[-1])
         total += merged
