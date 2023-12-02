0a9441c67c03aa1f0f5661c0319e9d91b4a51984
vaarg <104344158+vaarg@users.noreply.github.com>
2023-12-02 14:31:48 +1100

    Create day_1B.py

---
+++ day_1B.py
@@ -0,0 +1,20 @@
+import re
+
+def replaceStringsWithNumbers(line):
+    replacements = {
+        "one": "o1e", "two": "t2o", "three": "t3e", "four": "f4r", 
+        "five": "f5e", "six": "s6x", "seven": "s7n", "eight": "e8t", "nine": "n9e"
+    }
+    for word, number in replacements.items():
+        line = re.sub(word, number, line)
+    return line
+
+total = 0
+with open("day_1.txt", "r") as file:
+    for line in file:
+        line = replaceStringsWithNumbers(line)
+        line = re.sub("[a-z]", "", line.strip())
+        merged = int(line[0] + line[-1])
+        total += merged
+
+print(total)
