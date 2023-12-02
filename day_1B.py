d5b24231312c34937cede33b0e1dd10aea174eda
vaarg <104344158+vaarg@users.noreply.github.com>
2023-12-02 14:59:50 +1100

    Update day_1B.py

--- day_1B.py
+++ day_1B.py
@@ -1,9 +1,15 @@
-# Q: "Why is "o1e", "t2o", etc., being used?
-# A: As "three" is replaced first, if the dictionary replaces it with "3" 
-# then the string becomes "eigh37" which will give 37, which is incorrect.
-# However, if we replace "three" with "t3e" the string becomes "eight3e7"
-# which will then become "83e7" which will give "87" correctly.
+'''
+Q: "Why are we using "o1e", "t2o", "t3e", etc.?
 
+A: Take the example of "eighthree7":
+    As "three" is replaced first, if we directly replace it with "3" 
+    then the string becomes "eigh37" which will give 37, ignoring the original
+    "eight" and thus producing the incorrect result.
+    
+    However, if we replace "three" with "t3e" the string becomes "eight3e7"
+    which still allows the "eight" to be changed into a digit. The will then 
+    become "e8t3e7" which will give "87" correctly.
+'''
 import re
 
 def convert_to_numeric(line):
